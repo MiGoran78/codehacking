56644d46c8369e6e06fc37c51739e7f3d4d4d043
migoran78 <mi.goran@hotmail.com>
2016-11-28 23:14:53 +0100

    28/11-3

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,39 @@
 @extends('layouts.admin')
 
+
 @section('content')
 
     <h1>Posts</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>User</th>
+                <th>Category</th>
+                <th>Photo</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created</th>
+                <th>Updated</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if($posts)
+                @foreach($posts as $post)
+                    <tr>
+                        <th>{{$post->id}}</th>
+                        <th>{{$post->user_id}}</th>
+                        <th>{{$post->category_id}}</th>
+                        <th>{{$post->photo_id}}</th>
+                        <th>{{$post->title}}</th>
+                        <th>{{$post->body}}</th>
+                        <th>{{$post->created_at->diffForHumans()}}</th>
+                        <th>{{$post->updated_at->diffForHumans()}}</th>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
 
 @stop
