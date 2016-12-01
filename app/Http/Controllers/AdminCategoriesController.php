13a4319937cd82c9cb7abae1877d734ae3d316cb
migoran78 <mi.goran@hotmail.com>
2016-12-01 22:26:59 +0100

    categories finished

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -71,7 +71,8 @@ class AdminCategoriesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::findOrFail($id);
+        return view('admin.categories.edit', compact('category'));
     }
 
 
@@ -85,7 +86,10 @@ class AdminCategoriesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->update($request->all());
+
+        return redirect('/admin/categories');
     }
 
 
@@ -98,6 +102,7 @@ class AdminCategoriesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Category::findOrFail($id)->delete();
+        return redirect('/admin/categories');
     }
 }
